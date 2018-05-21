ecf97c2a77e57ac3c32181ea6ee6e8834d0025b8
DESKTOP-29TQDDA\Vlad Alexandru MEICI <vlad.meici@gmail.com>
2018-05-21 21:21:00 +0300

    planning calendar

--- ClockMe/App_Start/BundleConfig.cs
+++ ClockMe/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace ClockMe
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/bootstrap-year-calendar.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-year-calendar.css"));
         }
     }
 }
